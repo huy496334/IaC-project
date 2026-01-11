e722da285eac15da4eda9443201571c747bdcf73
huy496334 <496334@student.fontys.nl>
2026-01-11 08:29:49 +0100

    Gave router 2 GB of RAM

--- vms.tf
+++ vms.tf
@@ -2,6 +2,8 @@
 
 # Wazuh SIEM Server
 resource "proxmox_vm_qemu" "wazuh_server" {
+  depends_on = [proxmox_vm_qemu.router]
+  
   name            = "wazuh-server"
   vmid            = 151
   target_node     = var.proxmox_node
@@ -72,6 +74,8 @@ resource "proxmox_vm_qemu" "wazuh_server" {
 
 # Suricata IDS Server
 resource "proxmox_vm_qemu" "suricata_ids" {
+  depends_on = [proxmox_vm_qemu.router]
+  
   name            = "suricata-ids"
   vmid            = 152
   target_node     = var.proxmox_node
@@ -142,6 +146,8 @@ resource "proxmox_vm_qemu" "suricata_ids" {
 
 # Zabbix + Grafana Monitoring Server
 resource "proxmox_vm_qemu" "zabbix_grafana" {
+  depends_on = [proxmox_vm_qemu.router]
+  
   name            = "zabbix-grafana"
   vmid            = 153
   target_node     = var.proxmox_node
@@ -212,6 +218,8 @@ resource "proxmox_vm_qemu" "zabbix_grafana" {
 
 # GLPI Ticketing System
 resource "proxmox_vm_qemu" "glpi_tickets" {
+  depends_on = [proxmox_vm_qemu.router]
+  
   name            = "glpi-tickets"
   vmid            = 154
   target_node     = var.proxmox_node
@@ -282,6 +290,8 @@ resource "proxmox_vm_qemu" "glpi_tickets" {
 
 # T-Pot Honeypot Server
 resource "proxmox_vm_qemu" "tpot_honeypot" {
+  depends_on = [proxmox_vm_qemu.router]
+  
   name            = "tpot-honeypot"
   vmid            = 155
   target_node     = var.proxmox_node
@@ -352,6 +362,8 @@ resource "proxmox_vm_qemu" "tpot_honeypot" {
 
 # Infection Monkey Server (in SOC Network for testing)
 resource "proxmox_vm_qemu" "infection_monkey" {
+  depends_on = [proxmox_vm_qemu.router]
+  
   name            = "infection-monkey"
   vmid            = 156
   target_node     = var.proxmox_node
@@ -415,6 +427,103 @@ resource "proxmox_vm_qemu" "infection_monkey" {
       user        = var.ssh_username
       private_key = file("~/.ssh/id_ed25519")
       host        = "192.168.50.40"
+      timeout     = "5m"
+    }
+  }
+}
+# Router VM for VLAN Routing
+resource "proxmox_vm_qemu" "router" {
+  name            = "router"
+  vmid            = 150
+  target_node     = var.proxmox_node
+  clone           = var.template_name
+  full_clone      = true
+  
+  memory          = 2048
+  scsihw          = "virtio-scsi-pci"
+  boot            = "order=scsi0"
+  
+  serial {
+    id   = 0
+    type = "socket"
+  }
+  
+  vga {
+    type = "serial0"
+  }
+  
+  cpu {
+    cores = var.vm_cores * 2
+  }
+  
+  disk {
+    type    = "disk"
+    storage = var.storage_pool
+    size    = var.vm_disk_size
+    slot    = "scsi0"
+  }
+  
+  disk {
+    type    = "cloudinit"
+    storage = var.storage_pool
+    slot    = "scsi2"
+  }
+  
+  # Management interface (vmbr0) - DHCP
+  network {
+    id     = 0
+    model  = "virtio"
+    bridge = "vmbr0"
+  }
+  
+  # SOC Network (vmbr50) - VLAN 50
+  network {
+    id     = 1
+    model  = "virtio"
+    bridge = var.soc_bridge
+    tag    = var.soc_network_vlan
+  }
+  
+  # Honeypot Network (vmbr52) - VLAN 52
+  network {
+    id     = 2
+    model  = "virtio"
+    bridge = var.honeypot_bridge
+    tag    = var.honeypot_network_vlan
+  }
+  
+  ciuser = var.ssh_username
+  cipassword = var.ssh_password
+  sshkeys = var.ssh_public_key
+  ipconfig0 = "ip=192.168.1.201/24,gw=192.168.1.1"
+  ipconfig1 = "ip=192.168.50.254/24"
+  ipconfig2 = "ip=192.168.52.254/24"
+  start_at_node_boot = true
+  
+  provisioner "remote-exec" {
+    inline = [
+      "sudo apt update",
+      "sudo apt install -y qemu-guest-agent net-tools isc-dhcp-server",
+      "sudo apt upgrade -y",
+      "sudo sysctl -w net.ipv4.ip_forward=1",
+      "echo 'net.ipv4.ip_forward=1' | sudo tee -a /etc/sysctl.conf",
+      "sleep 5",
+      "sudo ip addr add 192.168.50.254/24 dev eth1 || true",
+      "sudo ip addr add 192.168.52.254/24 dev eth2 || true",
+      "sudo ip route add 192.168.50.0/24 via 192.168.50.254 dev eth1 || true",
+      "sudo ip route add 192.168.52.0/24 via 192.168.52.254 dev eth2 || true",
+      "sudo ufw --force enable || true",
+      "sudo ufw allow from 192.168.50.0/24 to 192.168.52.0/24 || true",
+      "sudo ufw allow from 192.168.52.0/24 to 192.168.50.0/24 || true",
+      "sudo systemctl start qemu-guest-agent",
+      "sudo systemctl enable qemu-guest-agent"
+    ]
+    
+    connection {
+      type        = "ssh"
+      user        = var.ssh_username
+      private_key = file("~/.ssh/id_ed25519")
+      host        = "192.168.1.201"
       timeout     = "5m"
     }
   }
